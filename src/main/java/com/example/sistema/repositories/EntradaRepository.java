e044cc47d159894ce43652386b5ea434420c1503
Diego Petrucci <diegodearaujopetrucci@outlook.com>
2024-06-03 03:58:57 -0300

    funcionando

--- src/main/java/com/example/sistema/repositories/EntradaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sistema.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.sistema.models.Entrada;
-
-
-public interface  EntradaRepository extends JpaRepository<Entrada, Long>{
-}
--- src/main/java/com/example/sistema/repositories/ItemEntradaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sistema.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.sistema.models.ItemEntrada;
-
-
-public interface  ItemEntradaRepository extends JpaRepository<ItemEntrada, Long>{
-}
